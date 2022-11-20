3c77d86715ffb5a51edc31073ea11df4726755d0
Alexandre Artisien | RouleMarcel <alexandre.artisien@gmail.com>
2022-11-20 20:09:55 +0100

    Scroll to top !

--- frontend/src/components/Utils.js
+++ frontend/src/components/Utils.js
@@ -3,7 +3,6 @@ export const dateParser = (num) => {
   let options = {
     hour: "2-digit",
     minute: "2-digit",
-    second: "2-digit",
     weekday: "long",
     year: "numeric",
     month: "short",
